bcbc13ba57f3b8f299d89de8615d7e2624e0d148
Matias <fernando.matias.duarte@gmail.com>
2023-01-23 00:07:31 -0300

    Test 1

---
+++ src/com/example/Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int numero = 10;
+        float numeroFlotante = 0.0f;
+        double numeroDoble = 27.0;
+        String nombre = "Pablo";
+        long numeroLargo = 12341234;
+        boolean condiconal = false;
+
+        /*
+        A continuacion se imprimiran los valores de las variables
+        @MattyDroid sera el usuario generado en github
+         */
+
+        System.out.println(numero);
+        System.out.println(numeroFlotante);
+        System.out.println(numeroDoble);
+        System.out.println(numeroLargo);
+        System.out.println(nombre);
+        System.out.println(condiconal);
+
+        //Este es un comentario
+    }
+}
